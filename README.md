19d65a0738888b5e4d85b9b1bea14a9d301933bc
ethmth <ethan@smaze.net>
2023-01-14 18:40:02 -0500

    ipcheck.sh successfully reports VPN IP and PORT

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pia-ip
+
+Can update ZoneEditDDNS every 10 minutes, so run crontab every 11? 
--- ipcheck.sh
+++ ipcheck.sh
@@ -15,81 +15,82 @@ function get_portforward
 	piactl get portforward
 }
 
+# SEND IFTTT REQUEST
+function ifttt_request
+{
+	curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"local-ip\": \"${LOCAL_IP}\",\"vpn-ip\": \"${VPN_IP}\",\"vpn-port\": \"${VPN_PORT}\"}" https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_KEY}
+	echo "Running curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"local-ip\": \"${LOCAL_IP}\",\"vpn-ip\": \"${VPN_IP}\",\"vpn-port\": \"${VPN_PORT}\"}" https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_KEY}"
+}
+
 # CHECK FOR PIA VPN IP
 VPN_IP=$(get_vpn_ip)
-MAX_CHECKS=20
+MAX_CHECKS=10
 CHECKS=0
 
 while [ "$VPN_IP" = "Unknown" ]; do
-	
-    sleep 10;
-    VPN_IP=$(get_vpn_ip)
-
-    CHECKS=$[ $CHECKS + 1 ]
-    if [ $CHECKS -gt $MAX_CHECKS ]; then
-        break
-    fi
+	sleep 5;
+	VPN_IP=$(get_vpn_ip)
+	CHECKS=$[ $CHECKS + 1 ]
+	if [ $CHECKS -gt $MAX_CHECKS ]; then
+		break
+	fi
 done
 
 if [ "$VPN_IP" = "Unknown" ]; then
-    exit 1
+	exit 1
 fi
 
 
 # CHECK FOR PIA FORWARDED PORT
-PORTFORWARD=$(get_portforward)
+VPN_PORT=$(get_portforward)
 MAX_CHECKS=3
 CHECKS=0
 
-while [ "$PORTFORWARD" = "Inactive" ]; do
-
-    sleep 5;
-	PORTFORWARD=$(get_portforward)
-
-    CHECKS=$[ $CHECKS + 1 ]
-    if [ $CHECKS -gt $MAX_CHECKS ]; then
-        break
-    fi
+while [ "$VPN_PORT" = "Inactive" ]; do
+	sleep 3;
+	VPN_PORT=$(get_portforward)
+	CHECKS=$[ $CHECKS + 1 ]
+	if [ $CHECKS -gt $MAX_CHECKS ]; then
+		break
+	fi
 done
 
-if [ "$PORTFORWARD" = "Inactive" ]; then
-    exit 1
-fi
-
-echo "vpn ip: $VPN_IP, forwarded port: $PORTFORWARD"
-
 # GET THE LOCAL IP
+source "${ABSOLUTE_PATH}/.env"
+LOCAL_INET=$(ip a | grep ${INTERFACE_NAME} | grep inet | xargs)
+LOCAL_INET=($LOCAL_INET)
+LOCAL_IP=${LOCAL_INET[1]}
 
-#source "${ABSOLUTE_PATH}/.env"
-
-#IP_ADDRESS=$(ip a | grep ${INTERFACE_NAME} | grep inet)
-
-#echo ${IP_ADDRESS} > ${ABSOLUTE_PATH}/ip_temp.txt
-
-#IP_ADDRESS=$(cat ${ABSOLUTE_PATH}/ip_temp.txt)
-
-
+# UPDATE ZONEEDIT DDNS (OPTIONAL)
+# curl https://$ZONEEDIT_USERNAME:$ZONEEDIT_DYN_KEY@dynamic.zoneedit.com/auth/dynamic.html?host=$ZONEEDIT_HOSTNAME&myip=$VPN_IP
 
 # SEND THE LOCAL IP TO IFTTT ON startup ARG
-
 if [[ "$1" == "startup" ]]; then
-    curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"ip-info\": \"${IP_ADDRESS}\"}" https://maker.ifttt.com/trigger/${EVENT_NAME}/json/with/key/${IFTTT_KEY}
-
-    echo ${IP_ADDRESS} > ${ABSOLUTE_PATH}/ip_state.txt
-
-    exit 0
+	ifttt_request
+	echo ${LOCAL_IP} > ${ABSOLUTE_PATH}/.ip_local.temp
+	echo ${VPN_IP} > ${ABSOLUTE_PATH}/.vpn_ip.temp
+	echo ${VPN_PORT} > ${ABSOLUTE_PATH}/.vpn_port.temp
+	exit 0
 fi
 
 # COMPARE THE NEW IP TO THE OLD IP ON NON-STARTUP FLAG
+OLD_LOCAL_IP=$(cat ${ABSOLUTE_PATH}/.ip_local.temp)
+OLD_VPN_IP=$(cat ${ABSOLUTE_PATH}/.vpn_ip.temp)
+OLD_VPN_PORT=$(cat ${ABSOLUTE_PATH}/.vpn_port.temp)
 
-OLD_IP_ADDRESS=$(cat ${ABSOLUTE_PATH}/ip_state.txt)
-
-if [[ "$OLD_IP_ADDRESS" == "$IP_ADDRESS" ]]; then
-    echo "ip hasn't changed"
+if [[ "$OLD_LOCAL_IP" != "$LOCAL_IP" ]]; then
+	ifttt_request
+elif [[ "$OLD_VPN_IP" != "$VPN_IP" ]]; then
+	ifttt_request
+elif [[ "$OLD_VPN_PORT" != "$VPN_PORT" ]]; then
+	ifttt_request
 else
-    curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"ip-info\": \"${IP_ADDRESS}\"}" https://maker.ifttt.com/trigger/${EVENT_NAME}/json/with/key/${IFTTT_KEY}
+	echo "Nothing has changed"
+	exit 0
 fi
 
-echo ${IP_ADDRESS} > ${ABSOLUTE_PATH}/ip_state.txt
+echo ${LOCAL_IP} > ${ABSOLUTE_PATH}/.ip_local.temp
+echo ${VPN_IP} > ${ABSOLUTE_PATH}/.vpn_ip.temp
+echo ${VPN_PORT} > ${ABSOLUTE_PATH}/.vpn_port.temp
 
 exit 0
