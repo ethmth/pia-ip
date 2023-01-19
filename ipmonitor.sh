f3e2d11a303beffe33a64e2a997da14b90d1dec6
ethmth <ethan@smaze.net>
2023-01-19 17:08:46 -0500

    ipmonitor.sh works

--- ipmonitor.sh
+++ ipmonitor.sh
@@ -16,11 +16,23 @@ function check_online
 	fi
 }
 
+# GET THE VPN CONNECTION STATUS AND PORT FROM PIACTL
+function get_vpn_ip
+{
+	/usr/local/bin/piactl get vpnip
+}
+
+function get_portforward
+{
+	/usr/local/bin/piactl get portforward
+}
+
+
 # SEND IFTTT REQUEST
 function ifttt_request
 {
 	curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"message\": \"${IFTTT_MESSAGE}\",\"local-ip\": \"${LOCAL_IP}\",\"vpn-ip\": \"${VPN_IP}\",\"vpn-port\": \"${VPN_PORT}\"}" https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_KEY}
-	echo "Running curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"local-ip\": \"${LOCAL_IP}\",\"vpn-ip\": \"${VPN_IP}\",\"vpn-port\": \"${VPN_PORT}\"}" https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_KEY}"
+	echo "Running curl -o /dev/null -X POST -H \"Content-Type: application/json\" -d \"{\"message\": \"${IFTTT_MESSAGE}\",\"local-ip\": \"${LOCAL_IP}\",\"vpn-ip\": \"${VPN_IP}\",\"vpn-port\": \"${VPN_PORT}\"}\" https://maker.ifttt.com/trigger/${IFTTT_EVENT}/json/with/key/${IFTTT_KEY}"
 }
 
 # CHECK FOR ONLINE STATUS
@@ -37,22 +49,35 @@ while [ "$IS_ONLINE" = "0" ]; do
 	fi
 done
 
-#######################
-## CALL THE IFTTT BEFORE YOU START THE LOOP FOR INITIAL CONNECTION
+# CHECK FOR PIA VPN IP
+VPN_IP=$(get_vpn_ip)
+MAX_CHECKS=2
+CHECKS=0
 
-source "${ABSOLUTE_PATH}/.env"
-LOCAL_INET=$(ip a | grep ${INTERFACE_NAME} | grep inet | xargs)
-LOCAL_INET=($LOCAL_INET)
-LOCAL_IP=${LOCAL_INET[1]}
+while ([ "$VPN_IP" = "Unknown" ] || [ "$VPN_IP" = "Attempting" ]); do
+	sleep 5;
+	VPN_IP=$(get_vpn_ip)
+	CHECKS=$[ $CHECKS + 1 ]
+	if [ $CHECKS -gt $MAX_CHECKS ]; then
+		break
+	fi
+done
 
-VPN_IP=$(/usr/local/bin/piactl get vpnip)
-VPN_PORT=$(/usr/local/bin/piactl get portforward)
+# CHECK FOR PIA FORWARDED PORT
+VPN_PORT=$(get_portforward)
+MAX_CHECKS=3
+CHECKS=0
 
+while ([ "$VPN_PORT" = "Inactive" ] || [ "$VPN_IP" = "Attempting" ]); do
+	sleep 1;
+	VPN_PORT=$(get_portforward)
+	CHECKS=$[ $CHECKS + 1 ]
+	if [ $CHECKS -gt $MAX_CHECKS ]; then
+		break
+	fi
+done
 
-ifttt_request
-SENT_AT=$SECONDS
-###############################
-
+SENT_AT=-5
 
 while true
 do
@@ -63,6 +88,8 @@ do
 			ELAPSED=$((SECONDS-SENT_AT))
 
 			if [ $ELAPSED -gt 2 ]; then
+
+				sleep 3
 
 				source "${ABSOLUTE_PATH}/.env"
 				LOCAL_INET=$(ip a | grep ${INTERFACE_NAME} | grep inet | xargs)
